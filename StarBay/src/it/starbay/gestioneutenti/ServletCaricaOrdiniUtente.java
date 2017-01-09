e606b88a4e82e6e6d50e1bde12ab5015e716d218
giandark <gianluca.fiscone@gmail.com>
2017-01-09 10:17:50 +0100

    Cambiata modo di prendere l'username

--- StarBay/src/it/starbay/gestioneutenti/ServletCaricaOrdiniUtente.java
+++ StarBay/src/it/starbay/gestioneutenti/ServletCaricaOrdiniUtente.java
@@ -8,7 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import it.starbay.gestionebean.Cliente;
 import it.starbay.gestionebean.Ordine;
 
 
@@ -18,9 +20,10 @@ public class ServletCaricaOrdiniUtente extends HttpServlet
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
 		ManagerUtenti mu = new ManagerUtenti();
-		String username = request.getHeader("username");
-		ArrayList<Ordine> ordini = mu.dammiOrdiniUtente(username);
-		request.getSession().setAttribute("ordini", ordini);
+		HttpSession session = request.getSession();
+		Cliente c = (Cliente) session.getAttribute("cliente");
+		ArrayList<Ordine> ordini = mu.dammiOrdiniUtente(c.getUsername());
+		session.setAttribute("ordini", ordini);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
