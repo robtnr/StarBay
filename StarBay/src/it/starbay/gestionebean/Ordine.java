e8c71f6d7cc2c3a313385a6826c5afcb935ffe9d
robtnr <roberto.cont94@gmail.com>
2017-01-09 12:06:04 +0100

    Modificato il bean Ordine

--- StarBay/src/it/starbay/gestionebean/Ordine.java
+++ StarBay/src/it/starbay/gestionebean/Ordine.java
@@ -3,6 +3,8 @@ package it.starbay.gestionebean;
 public class Ordine 
 {
 	private int idOrdine;
+	private String username;
+	private String descrizione;
 	private String data;
 	private String ora;
 	private int idDettaglioOrdine;
@@ -10,79 +12,117 @@ public class Ordine
 	private double prezzo;
 	private String idProdotto;
 	private String nomeProdotto;
-	public String getNomeProdotto() {
-		return nomeProdotto;
-	}
-
-	public void setNomeProdotto(String nomeProdotto) {
-		this.nomeProdotto = nomeProdotto;
-	}
-
 	private String tipo;
 	
 	public Ordine() {}
+	
+	public String getUsername()
+	{
+		return username;
+	}
 
-	public int getIdOrdine() {
+	public String getDescrizione() 
+	{
+		return descrizione;
+	}
+
+	public void setUsername(String username) 
+	{
+		this.username = username;
+	}
+
+	public void setDescrizione(String descrizione) 
+	{
+		this.descrizione = descrizione;
+	}
+	
+	public String getNomeProdotto() 
+	{
+		return nomeProdotto;
+	}
+
+	public void setNomeProdotto(String nomeProdotto) 
+	{
+		this.nomeProdotto = nomeProdotto;
+	}
+
+	public int getIdOrdine() 
+	{
 		return idOrdine;
 	}
 
-	public void setIdOrdine(int idOrdine) {
+	public void setIdOrdine(int idOrdine)
+	{
 		this.idOrdine = idOrdine;
 	}
 
-	public String getData() {
+	public String getData() 
+	{
 		return data;
 	}
 
-	public void setData(String data) {
+	public void setData(String data)
+	{
 		this.data = data;
 	}
 
-	public String getOra() {
+	public String getOra() 
+	{
 		return ora;
 	}
 
-	public void setOra(String ora) {
+	public void setOra(String ora)
+	{
 		this.ora = ora;
 	}
 
-	public int getIdDettaglioOrdine() {
+	public int getIdDettaglioOrdine()
+	{
 		return idDettaglioOrdine;
 	}
 
-	public void setIdDettaglioOrdine(int idDettaglioOrdine) {
+	public void setIdDettaglioOrdine(int idDettaglioOrdine)
+	{
 		this.idDettaglioOrdine = idDettaglioOrdine;
 	}
 
-	public int getQuantita() {
+	public int getQuantita() 
+	{
 		return quantita;
 	}
 
-	public void setQuantita(int quantita) {
+	public void setQuantita(int quantita)
+	{
 		this.quantita = quantita;
 	}
 
-	public double getPrezzo() {
+	public double getPrezzo() 
+	{
 		return prezzo;
 	}
 
-	public void setPrezzo(double prezzo) {
+	public void setPrezzo(double prezzo) 
+	{
 		this.prezzo = prezzo;
 	}
 
-	public String getIdProdotto() {
+	public String getIdProdotto() 
+	{
 		return idProdotto;
 	}
 
-	public void setIdProdotto(String idProdotto) {
+	public void setIdProdotto(String idProdotto) 
+	{
 		this.idProdotto = idProdotto;
 	}
 
-	public String getTipo() {
+	public String getTipo() 
+	{
 		return tipo;
 	}
 
-	public void setTipo(String tipo) {
+	public void setTipo(String tipo) 
+	{
 		this.tipo = tipo;
 	}
 	
