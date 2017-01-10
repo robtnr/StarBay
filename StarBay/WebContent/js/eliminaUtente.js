2cf6b52e2ca3b411a6eb70d2030797feb4273729
robtnr <roberto.cont94@gmail.com>
2017-01-10 12:36:50 +0100

    Modificato eliminaUtente.js, aggiunto il refresh

--- StarBay/WebContent/js/eliminaUtente.js
+++ StarBay/WebContent/js/eliminaUtente.js
@@ -31,12 +31,17 @@ function eliminaUtente(x)
 		if (this.readyState == 4 && this.status == 200) 
 		{
 			swal({
-				title: "Utente eliminato",
-				type: "success",
-				showCancelButton: false,
-				closeOnConfirm: false,
-				showLoaderOnConfirm: true,
-			});
+				  title: "Utente eliminato con successo!",
+				  type: "success",
+				  showCancelButton: false,
+				  confirmButtonText: "OK",
+				  closeOnConfirm: false,
+				},
+				function(isConfirm){
+				  if (isConfirm) {
+					  open("elimina_utente.jsp","_self");
+				  }
+				});
 		}
 	};
 	http.open("GET", "ServletEliminaUtente", true);
