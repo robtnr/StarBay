a9f34fd4764cb1a079b5be9633c3469158b3c52e
robtnr <roberto.cont94@gmail.com>
2017-01-11 15:04:39 +0100

    Creato il test per il Manager Navigazione

---
+++ StarBay/testing/it/starbay/manager/ManagerNavigazioneTest.java
@@ -0,0 +1,44 @@
+package it.starbay.manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ManagerNavigazioneTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCaricaUltimiProdotti() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCaricaProdottiStella() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCaricaProdottiStore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCaricaProdotti() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCercaProdotti() {
+		fail("Not yet implemented");
+	}
+
+}
