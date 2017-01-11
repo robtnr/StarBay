381f073b674df6a129de93fa9bf3a0def997719c
robtnr <roberto.cont94@gmail.com>
2017-01-11 15:05:56 +0100

    Creato il test per il Manager Utenti

---
+++ StarBay/testing/it/starbay/manager/ManagerUtentiTest.java
@@ -0,0 +1,44 @@
+package it.starbay.manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ManagerUtentiTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRegistraUtente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testControlloCredenziali() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDammiOrdiniUtente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDammiClienti() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEliminaUtente() {
+		fail("Not yet implemented");
+	}
+
+}
