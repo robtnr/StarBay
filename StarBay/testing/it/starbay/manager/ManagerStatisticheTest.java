c392e8e59bbe0b055d8ad2b611e49dc122468d10
robtnr <roberto.cont94@gmail.com>
2017-01-11 15:05:39 +0100

    Creato il test per il Manager Statistiche

---
+++ StarBay/testing/it/starbay/manager/ManagerStatisticheTest.java
@@ -0,0 +1,34 @@
+package it.starbay.manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ManagerStatisticheTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDammiGuadagnoTotale() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDammiRicavoTotale() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDammiGuadagnoSingoloProdotto() {
+		fail("Not yet implemented");
+	}
+
+}
