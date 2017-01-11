8e719fe0929b2cdea50608e73a449a42d819d144
robtnr <roberto.cont94@gmail.com>
2017-01-11 15:05:12 +0100

    Creato il test per il Manager Prodotti

---
+++ StarBay/testing/it/starbay/manager/ManagerProdottiTest.java
@@ -0,0 +1,49 @@
+package it.starbay.manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ManagerProdottiTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRegistraProdottoStella() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegistraProdottoStore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModificaProdottoStore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModificaProdottoStella() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEliminaStella() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEliminaProdottoStore() {
+		fail("Not yet implemented");
+	}
+
+}
