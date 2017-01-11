5aea0863a7b42ff87a533d406f4511cea01b7d5b
robtnr <roberto.cont94@gmail.com>
2017-01-11 15:00:26 +0100

    Creato il test per ManagerAcquisti

---
+++ StarBay/testing/it/starbay/manager/ManagerAcquistiTest.java
@@ -0,0 +1,64 @@
+package it.starbay.manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ManagerAcquistiTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetQuantitaProdotto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCountOrdine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreaOrdine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCountDettaglioOrdine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreaDettagliOrdine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreaIncludeStella() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCoordinate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreaIncludeStore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAggiornaQuantitaNomeProdotto() {
+		fail("Not yet implemented");
+	}
+
+}
