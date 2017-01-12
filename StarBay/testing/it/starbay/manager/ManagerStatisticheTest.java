aad701e276cd808c81026eff8e31120a0d23226d
giandark <gianluca.fiscone@gmail.com>
2017-01-12 16:11:57 +0100

    Implementato il test sul ManagerStatistiche

--- StarBay/testing/it/starbay/manager/ManagerStatisticheTest.java
+++ StarBay/testing/it/starbay/manager/ManagerStatisticheTest.java
@@ -6,29 +6,40 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import it.starbay.gestionestatistiche.ManagerStatistiche;
+
 public class ManagerStatisticheTest {
 
+	private ManagerStatistiche ms;
+
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception 
+	{
+		ms = new ManagerStatistiche();
 	}
 
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception 
+	{
+		ms = null;
 	}
 
 	@Test
-	public void testDammiGuadagnoTotale() {
-		fail("Not yet implemented");
+	public void testDammiGuadagnoTotale() 
+	{
+		assertNotNull(ms.dammiGuadagnoTotale());
 	}
 
 	@Test
-	public void testDammiRicavoTotale() {
-		fail("Not yet implemented");
+	public void testDammiRicavoTotale() 
+	{
+		assertNotNull(ms.dammiRicavoTotale());
 	}
 
 	@Test
-	public void testDammiGuadagnoSingoloProdotto() {
-		fail("Not yet implemented");
+	public void testDammiGuadagnoSingoloProdotto() 
+	{
+		assertNotNull(ms.dammiGuadagnoSingoloProdotto());
 	}
 
 }
