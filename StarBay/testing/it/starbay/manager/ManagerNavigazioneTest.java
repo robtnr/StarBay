70c596c9d3ca28cb07859b6ae7895140be702ef9
giandark <gianluca.fiscone@gmail.com>
2017-01-12 12:38:16 +0100

    Implementato il test sul ManagerNavigazione

--- StarBay/testing/it/starbay/manager/ManagerNavigazioneTest.java
+++ StarBay/testing/it/starbay/manager/ManagerNavigazioneTest.java
@@ -2,43 +2,67 @@ package it.starbay.manager;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import it.starbay.gestionebean.Stella;
+import it.starbay.gestionebean.Store;
+import it.starbay.gestionenavigazione.ManagerNavigazione;
+
 public class ManagerNavigazioneTest {
+
+	private ManagerNavigazione mn;
 
 	@Before
 	public void setUp() throws Exception {
+		mn = new ManagerNavigazione();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		mn = null;
 	}
 
 	@Test
-	public void testCaricaUltimiProdotti() {
-		fail("Not yet implemented");
+	public void testCaricaUltimiProdotti() 
+	{
+		ArrayList<Store> store = new ArrayList<Store>();
+		store = mn.caricaUltimiProdotti("store");
+		assertNotNull(store);
 	}
 
 	@Test
-	public void testCaricaProdottiStella() {
-		fail("Not yet implemented");
+	public void testCaricaProdottiStella() 
+	{
+		ArrayList<Stella> stelle = new ArrayList<Stella>();
+		stelle = mn.caricaProdottiStella();
+		assertNotNull(stelle);
 	}
 
 	@Test
-	public void testCaricaProdottiStore() {
-		fail("Not yet implemented");
+	public void testCaricaProdottiStore() 
+	{
+		ArrayList<Store> store = new ArrayList<Store>();
+		store = mn.caricaProdottiStore();
+		assertNotNull(store);
 	}
 
 	@Test
-	public void testCaricaProdotti() {
-		fail("Not yet implemented");
+	public void testCaricaProdotti() 
+	{
+		ArrayList<Store> store = new ArrayList<Store>();
+		store = mn.caricaProdotti("store");
+		assertNotNull(store);
 	}
 
 	@Test
-	public void testCercaProdotti() {
-		fail("Not yet implemented");
+	public void testCercaProdotti() 
+	{
+		ArrayList prodotti_trovati = new ArrayList(); 
+		prodotti_trovati = mn.cercaProdotti("Melissa");
+		assertNotNull(prodotti_trovati);
 	}
-
 }
